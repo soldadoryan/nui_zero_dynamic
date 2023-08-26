42632a6f74c715275c367d8b8ba65f6b04311f70
Ryan Drumond <ryandrumond@ioasys.com.br>
2023-08-26 20:05:22 -0300

    a

--- src/hooks/useRequest.js
+++ src/hooks/useRequest.js
@@ -1,6 +1,6 @@
 function useRequest() {
   const request = async (method, body = {}) => {
-    return fetch(`http://zero_dynamic/${method}`, {
+    return fetch(`https://104.234.189.131/zero_dynamic/${method}`, {
       method: "POST",
       body: JSON.stringify(body),
     });
